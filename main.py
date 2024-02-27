ba7ad441f3911fa0f4cad1f2d8afb47763d6406b
Luke Birchmeier <147448121+Birchroot@users.noreply.github.com>
2024-02-26 22:02:48 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,58 +1,56 @@
-# Jacob if you want to rewrite all this idc==========
+# Jacob if you want to rewrite any this idc==========
+commands = [
+    '1N', '2E', '3S'
+]
 # moveFoward/L/R function declarations ===============
+
+
 # array looper=======================================
-"""
-ok so when i tried to convert
-my js (technically typescript) 
-to python it wouldn't let me, i had to put the empty
-switch-case statements in a comment, but then everything 
-got scrambed when i converted languages. the original
-javascript (which is errorless) is on the Robot Tour 
-google doc I shared w/ u. maybe i'll fix this mess 
-later or you can if you want.
-"""
-commands = ['1N', '2E', '3S']
 def commandInterpreter(cmds: List[str]):
     previousDirection = 'N'
-    """
-    switch(splitCommand[1]){
-    case 'N':
-    switch(previousDirection){
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    case 'E':
-    switch (previousDirection) {
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    case 'S':
-    switch (previousDirection) {
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    case 'W':
-    switch (previousDirection) {
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    }
-    """
     for x in cmds:
         splitCommand = x.split("")
         distanceToMove = splitCommand[0]
-commandInterpreter(commands)
-# =========testing stuff bla bla========================
-"""
+        """
+        here's my java/type script for the switch case statements.
+        it wouldn't let me convert to python with them empty
+        so i will just put it here for you or me to fix later
+        switch(splitCommand[1]){
+            case 'N':
+                switch(previousDirection){
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'E':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'S':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'W':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
 
+        """
+commandInterpreter(commands)
+
+# =========testing stuff bla bla========================
+
+"""
 while (true) {
 makerbit.runMotor(MakerBitMotor.All, 100)
 time = control.millis()
--- main.ts
+++ main.ts
@@ -1,65 +1,56 @@
-//  Jacob if you want to rewrite all this idc==========
+//  Jacob if you want to rewrite any this idc==========
+let commands = ["1N", "2E", "3S"]
 //  moveFoward/L/R function declarations ===============
 //  array looper=======================================
-/** 
-ok so when i tried to convert
-my js (technically typescript) 
-to python it wouldn't let me, i had to put the empty
-switch-case statements in a comment, but then everything 
-got scrambed when i converted languages. the original
-javascript (which is errorless) is on the Robot Tour 
-google doc I shared w/ u. maybe i'll fix this mess 
-later or you can if you want.
-
- */
-let commands = ["1N", "2E", "3S"]
 function commandInterpreter(cmds: string[]) {
     let splitCommand: string[];
     let distanceToMove: string;
     let previousDirection = "N"
-    /** 
-    switch(splitCommand[1]){
-    case 'N':
-    switch(previousDirection){
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    case 'E':
-    switch (previousDirection) {
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    case 'S':
-    switch (previousDirection) {
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    case 'W':
-    switch (previousDirection) {
-    case 'N':
-    case 'E':
-    case 'S':
-    case 'W':
-    }
-    }
-    
- */
     for (let x of cmds) {
         splitCommand = _py.py_string_split(x, "")
         distanceToMove = splitCommand[0]
+        /** 
+        here's my java/type script for the switch case statements.
+        it wouldn't let me convert to python with them empty
+        so i will just put it here for you or me to fix later
+        switch(splitCommand[1]){
+            case 'N':
+                switch(previousDirection){
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'E':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'S':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'W':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+
+        
+ */
     }
 }
 
 commandInterpreter(commands)
 //  =========testing stuff bla bla========================
 /** 
-
 while (true) {
 makerbit.runMotor(MakerBitMotor.All, 100)
 time = control.millis()
