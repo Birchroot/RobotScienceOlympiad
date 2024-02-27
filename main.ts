ce7e581ad0000cf52685778561fa691913ff85df
Luke Birchmeier <147448121+Birchroot@users.noreply.github.com>
2024-02-26 21:45:13 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,65 @@
-let time: number;
-// moveFoward/L/R function declarations ===============
-// =====================================================
-while (true) {
-    makerbit.runMotor(MakerBitMotor.All, 100)
-    time = control.millis()
-    if (time > 3000) {
-        makerbit.stopMotor(MakerBitMotor.All)
+//  Jacob if you want to rewrite all this idc==========
+//  moveFoward/L/R function declarations ===============
+//  array looper=======================================
+/** 
+well frick I wrote this in javascript (technically ts)
+but it won't convert to python uh oh
+maybe it's just some stupid error idk
+if you know why that would be helpful. also if you
+wanna rewrite it all in python you can.
+
+commands = [
+    '1N', '2E', '3S'
+];
+function commandInterpreter(cmds: string[]){
+    let previousDirection = 'N';
+    for (const x of cmds){
+        let splitCommand = x.split("");
+        let distanceToMove = splitCommand[0];
+        switch(splitCommand[1]){
+            case 'N':
+                switch(previousDirection){
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'E':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'S':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+            case 'W':
+                switch (previousDirection) {
+                    case 'N':
+                    case 'E':
+                    case 'S':
+                    case 'W':
+                }
+        }
     }
-    
 }
+commandInterpreter(commands);
+
+ */
+//  =========testing stuff bla bla========================
+/** 
+while (true) {
+makerbit.runMotor(MakerBitMotor.All, 100)
+time = control.millis()
+if (time > 3000) {
+makerbit.stopMotor(MakerBitMotor.All)
+}
+
+}
+
+ */
