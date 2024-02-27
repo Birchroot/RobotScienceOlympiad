826b8462062e10311050638e2ccc6a0417be8759
Luke Birchmeier <147448121+Birchroot@users.noreply.github.com>
2024-02-26 21:54:11 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,59 +1,58 @@
 # Jacob if you want to rewrite all this idc==========
-
 # moveFoward/L/R function declarations ===============
-
 # array looper=======================================
 """
-well frick I wrote this in javascript (technically ts)
-but it won't convert to python uh oh
-maybe it's just some stupid error idk
-if you know why that would be helpful. also if you
-wanna rewrite it all in python you can.
-
-commands = [
-    '1N', '2E', '3S'
-];
-function commandInterpreter(cmds: string[]){
-    let previousDirection = 'N';
-    for (const x of cmds){
-        let splitCommand = x.split("");
-        let distanceToMove = splitCommand[0];
-        switch(splitCommand[1]){
-            case 'N':
-                switch(previousDirection){
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'E':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'S':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'W':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-        }
-    }
-}
-commandInterpreter(commands);
+ok so when i tried to convert
+my js (technically typescript) 
+to python it wouldn't let me, i had to put the empty
+switch-case statements in a comment, but then everything 
+got scrambed when i converted languages. the original
+javascript (which is errorless) is on the Robot Tour 
+google doc I shared w/ u. maybe i'll fix this mess 
+later or you can if you want.
 """
+commands = ['1N', '2E', '3S']
+def commandInterpreter(cmds: List[str]):
+    previousDirection = 'N'
+    """
+    switch(splitCommand[1]){
+    case 'N':
+    switch(previousDirection){
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    case 'E':
+    switch (previousDirection) {
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    case 'S':
+    switch (previousDirection) {
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    case 'W':
+    switch (previousDirection) {
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    }
+    """
+    for x in cmds:
+        splitCommand = x.split("")
+        distanceToMove = splitCommand[0]
+commandInterpreter(commands)
 # =========testing stuff bla bla========================
 """
+
 while (true) {
 makerbit.runMotor(MakerBitMotor.All, 100)
 time = control.millis()
@@ -62,4 +61,6 @@ makerbit.stopMotor(MakerBitMotor.All)
 }
 
 }
+
+
 """
--- main.ts
+++ main.ts
@@ -2,57 +2,64 @@
 //  moveFoward/L/R function declarations ===============
 //  array looper=======================================
 /** 
-well frick I wrote this in javascript (technically ts)
-but it won't convert to python uh oh
-maybe it's just some stupid error idk
-if you know why that would be helpful. also if you
-wanna rewrite it all in python you can.
-
-commands = [
-    '1N', '2E', '3S'
-];
-function commandInterpreter(cmds: string[]){
-    let previousDirection = 'N';
-    for (const x of cmds){
-        let splitCommand = x.split("");
-        let distanceToMove = splitCommand[0];
-        switch(splitCommand[1]){
-            case 'N':
-                switch(previousDirection){
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'E':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'S':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'W':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-        }
-    }
-}
-commandInterpreter(commands);
+ok so when i tried to convert
+my js (technically typescript) 
+to python it wouldn't let me, i had to put the empty
+switch-case statements in a comment, but then everything 
+got scrambed when i converted languages. the original
+javascript (which is errorless) is on the Robot Tour 
+google doc I shared w/ u. maybe i'll fix this mess 
+later or you can if you want.
 
  */
+let commands = ["1N", "2E", "3S"]
+function commandInterpreter(cmds: string[]) {
+    let splitCommand: string[];
+    let distanceToMove: string;
+    let previousDirection = "N"
+    /** 
+    switch(splitCommand[1]){
+    case 'N':
+    switch(previousDirection){
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    case 'E':
+    switch (previousDirection) {
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    case 'S':
+    switch (previousDirection) {
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    case 'W':
+    switch (previousDirection) {
+    case 'N':
+    case 'E':
+    case 'S':
+    case 'W':
+    }
+    }
+    
+ */
+    for (let x of cmds) {
+        splitCommand = _py.py_string_split(x, "")
+        distanceToMove = splitCommand[0]
+    }
+}
+
+commandInterpreter(commands)
 //  =========testing stuff bla bla========================
 /** 
+
 while (true) {
 makerbit.runMotor(MakerBitMotor.All, 100)
 time = control.millis()
@@ -61,5 +68,7 @@ makerbit.stopMotor(MakerBitMotor.All)
 }
 
 }
+
+
 
  */
