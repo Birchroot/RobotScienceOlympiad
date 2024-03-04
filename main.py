7d5a09961895e8bc0f6e6215e1b6926e2590dd1c
HunterThe9th <131089523+HunterThe9th@users.noreply.github.com>
2024-03-04 15:52:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,87 @@
-"""
-Jacob if you want to rewrite all this idc. when i tried to
-convert it to python it decided to break. og java/type script is
-on the google doc i shared w/ you (Robot Tour)
-"""
-#write on day of competition==========================
-commands = [
-    '1N', '2E', '3S'
-]
-#=====================================================
-
-# moveFoward/L/R function declarations ===============
-
-# array looper========================================
-
 def forward(strength):
-    print("WOW")
+    for x in range(strength):
+        print("FORWARD")
 
 def right():
-    print()
+    print("RIGHT")
 
 def left():
-    print("WOW")
+    print("LEFT")
 
+#Loops through and interprets the list 
 def commandInterpreter(cmds):
     previousDirection = 'N'
     for x in cmds:
         temp = list(x)
 
-        strength = temp[0]
+        strength = int(temp[0])
         direction = temp[1]
 
-        if(previousDirection == 'N'):
-            if(direction == "N"):
-                forward(strength)
-            elif(direction == "E"):
-                right()
-            elif(direction == "S"):
-                right()
-                right()
-            elif(direction == "W"):
-                left()
+        print("Prior direction: " +previousDirection)
+        print(strength)
+        print(direction)
 
-        if(previousDirection == 'E'):
-            if(direction == "N"):
-                left()
-            elif(direction == "E"):
+        if (previousDirection == 'N'):
+            if (direction == "N"):
                 forward(strength)
-            elif(direction == "S"):
+            elif (direction == "E"):
                 right()
-            elif(direction == "W"):
-                right()
-                right()
-
-        if(previousDirection == 'S'):
-            if(direction == "N"):
-                right()
-                right()
-            elif(direction == "E"):
-                left()
-            elif(direction == "S"):
                 forward(strength)
-            elif(direction == "W"):
+            elif (direction == "S"):
                 right()
-            
-        if(previousDirection == 'E'):
-            if(direction == "N"):
-                left()
-            elif(direction == "E"):
+                right()
                 forward(strength)
-            elif(direction == "S"):
-                right()
-            elif(direction == "W"):
-                right()
-                right()
-
-
-        if(previousDirection == 'W'):
-            if(direction == "N"):
-                right()
-            elif(direction == "E"):
-                right()
-                right()
-            elif(direction == "S"):
+            elif (direction == "W"):
                 left()
-            elif(direction == "W"):
                 forward(strength)
 
-            
+        if (previousDirection == 'E'):
+            if (direction == "N"):
+                left()
+                forward(strength)
+            elif (direction == "E"):
+                forward(strength)
+            elif (direction == "S"):
+                right()
+                forward(strength)
+            elif (direction == "W"):
+                right()
+                right()
+                forward(strength)
 
-        
+        if (previousDirection == 'S'):
+            if (direction == "N"):
+                right()
+                right()
+                forward(strength)
+            elif (direction == "E"):
+                left()
+                forward(strength)
+            elif (direction == "S"):
+                forward(strength)
+            elif (direction == "W"):
+                right()
+                forward(strength)
+
+        if (previousDirection == 'W'):
+            if (direction == "N"):
+                right()
+                forward(strength)
+            elif (direction == "E"):
+                right()
+                right()
+                forward(strength)
+            elif (direction == "S"):
+                left()
+                forward(strength)
+            elif (direction == "W"):
+                forward(strength)
+
+        previousDirection = direction
+
+#EDIT THIS FOR MOVEMENT
+commands = [
+    "1N", "2N","2E", "3S"
+]
 
 commandInterpreter(commands)
-
-
-
-
-# =========testing stuff bla bla========================
-"""
-
-while (true) {
-makerbit.runMotor(MakerBitMotor.All, 100)
-time = control.millis()
-if (time > 3000) {
-makerbit.stopMotor(MakerBitMotor.All)
-}
-
-}
-
-
-"""
