638491c47788b09483aaa5982899ce179541f99d
Luke Birchmeier <147448121+Birchroot@users.noreply.github.com>
2024-03-11 15:51:59 -0400

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,73 +1,17 @@
-/** 
-Jacob if you want to rewrite all this idc. when i tried to
-convert it to python it decided to break. og java/type script is
-on the google doc i shared w/ you (Robot Tour)
+let commands = ["1N", "2E", "100W"];
 
- */
-// write on day of competition==========================
-let commands = ["1N", "2E", "3S"]
-// =====================================================
-//  moveFoward/L/R function declarations ===============
-//  array looper========================================
-function commandInterpreter(cmds: string[]) {
-    let splitCommand: string[];
-    let distanceToMove: string;
-    let previousDirection = "N"
-    for (let x of cmds) {
-        splitCommand = _py.py_string_split(x, "")
-        distanceToMove = splitCommand[0]
-        /** these are the switch case statements from the 
-        java/typescript code that I wrote. it wouldn't
-        let me convert it to python with them empty. 
-        one of us can fix this some other time.
-        
-        switch (splitCommand[1]) {
-            case 'N':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'E':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'S':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-            case 'W':
-                switch (previousDirection) {
-                    case 'N':
-                    case 'E':
-                    case 'S':
-                    case 'W':
-                }
-        
- */
-    }
+
+function forward() {
+    motion.drive(100, 100)
 }
 
-commandInterpreter(commands)
-//  =========testing stuff bla bla========================
-/** 
-
-while (true) {
-makerbit.runMotor(MakerBitMotor.All, 100)
-time = control.millis()
-if (time > 3000) {
-makerbit.stopMotor(MakerBitMotor.All)
+function reverse(){
+    
 }
 
-}
-
-
-
- */
+commands.forEach((command) => {
+    let cmd = command.split("");
+    let strength = cmd[0];
+    let direction = cmd[1];
+    forward();
+})
