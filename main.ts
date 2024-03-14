94a9140071892f40908d668dc7c6e1f3e9450478
Luke Birchmeier <147448121+Birchroot@users.noreply.github.com>
2024-03-13 22:10:31 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,37 @@
-let commands = ["1N", "2E", "100W"];
+let commands = ["START", "1E"];
+
+let previousDirection = "N";
 
 let mu = 1;
+
 function forward25(strength: number) {
-    motion.drive(7.5 * mu, 1 * mu);
+    motion.drive(9 * mu, 1 * mu);
     control.waitMicros(300000);
-    motion.drive(7, 7);
+    motion.drive(8 * mu, 7 * mu);
     control.waitMicros(1290000);
     motion.stop();
 }
 
 function forward50(strength:number) {
-    motion.drive(8, 2);
+    motion.drive(8 * mu, 2 * mu);
     control.waitMicros(290000);
-    motion.drive(8, 7);
+    motion.drive(8 * mu, 7 * mu);
     control.waitMicros(800000);
-    motion.drive(9, 9);
+    motion.drive(9 * mu, 9 * mu);
     control.waitMicros(2000000);
     motion.stop();
+    
 }
 
 function right(){
-
+    motion.drive(0,5);
+    control.waitMicros(1000000);
+    motion.stop();
 }
 
 
 function left() {
-    motion.drive(100, 100)
+    
 }
 
 function reverse(){
@@ -33,89 +39,92 @@ function reverse(){
 }
 
 commands.forEach((command) => {
-    let cmd = command.split("");
-    let strength = parseInt(cmd[0]);
-    let direction = cmd[1];
-    let previousDirection = "N"
-    
-    if( previousDirection == 'N')
-    {
-        if(direction == "N")
+    if (command == 'START'){
+        forward25(1);
+    }else{
+        let cmd = command.split("");
+        let strength = parseInt(cmd[0]);
+        let direction = cmd[1];
+        
+        if(previousDirection == 'N')
         {
-            forward50(strength)
+            if(direction == "N")
+            {
+                forward50(strength)
+            }
+            else if(direction == "E")
+            {
+                right()
+                forward50(strength)
+            }
+            else if(direction == "S")
+            {
+                right()
+                right()
+                forward50(strength)
+            }
+            else if(direction == "W")
+            {
+                left()
+                forward50(strength)
+            }
         }
-        else if(direction == "E")
-        {
-            right()
-            forward50(strength)
-        }
-        else if(direction == "S")
-        {
-            right()
-            right()
-            forward50(strength)
-        }
-        else if(direction == "W")
-        {
-            left()
-            forward50(strength)
-        }
-    }
 
-    if (previousDirection == 'E') {
-        if (direction == "N") {
-            left()
-            forward50(strength)
+        if (previousDirection == 'E') {
+            if (direction == "N") {
+                left()
+                forward50(strength)
+            }
+            else if (direction == "E") {
+                forward50(strength)
+            }
+            else if (direction == "S") {
+                right()
+                forward50(strength)
+            }
+            else if (direction == "W") {
+                right()
+                right()
+                forward50(strength)
+            }
         }
-        else if (direction == "E") {
-            forward50(strength)
-        }
-        else if (direction == "S") {
-            right()
-            forward50(strength)
-        }
-        else if (direction == "W") {
-            right()
-            right()
-            forward50(strength)
-        }
-    }
 
-    if (previousDirection == 'S') {
-        if (direction == "N") {
-            right()
-            right()
-            forward50(strength)
+        if (previousDirection == 'S') {
+            if (direction == "N") {
+                right()
+                right()
+                forward50(strength)
+            }
+            else if (direction == "E") {
+                left()
+                forward50(strength)
+            }
+            else if (direction == "S") {
+                forward50(strength)
+            }
+            else if (direction == "W") {
+                right()
+                forward50(strength)
+            }
         }
-        else if (direction == "E") {
-            left()
-            forward50(strength)
-        }
-        else if (direction == "S") {
-            forward50(strength)
-        }
-        else if (direction == "W") {
-            right()
-            forward50(strength)
-        }
-    }
 
-    if (previousDirection == 'W') {
-        if (direction == "N") {
-            right()
-            forward50(strength)
-        }
-        else if (direction == "E") {
-            right()
-            right()
-            forward50(strength)
-        }
-        else if (direction == "S") {
-            left()
-            forward50(strength)
-        }
-        else if (direction == "W") {
-            forward50(strength)
+        if (previousDirection == 'W') {
+            if (direction == "N") {
+                right()
+                forward50(strength)
+            }
+            else if (direction == "E") {
+                right()
+                right()
+                forward50(strength)
+            }
+            else if (direction == "S") {
+                left()
+                forward50(strength)
+            }
+            else if (direction == "W") {
+                forward50(strength)
+            }
         }
     }
 })
